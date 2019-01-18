9231fd028d029482e1b3f41db6ae7c73e2100955
Christopher Kilinc <christopher.kilinc@if.se>
2019-01-18 11:17:47 +0100

    git init

---
+++ index.js
@@ -0,0 +1,27 @@
+export default class chronicleLogger {
+    constructor(_apiurl, _apikey){
+        const apiurl = _apiurl;
+        const apikey = _apikey;    
+    }
+
+    createLog = function(_system, _payload, _severity ) {
+        const dateFull = new Date(Date.now());
+        //  Create Log    
+        const log = {
+            logTime: dateFull.toISOString(),
+            system: _system,
+            severity: _severity,
+            details: {
+                payload: _payload
+            }
+        }
+        return log;
+    }
+
+    publishLog = function(log) {
+        const apiUrl = `${apiUrl}/q?=key=${apikey}`;    //  https://chronicleapi.horoku.com/q?=xxxxxxxxxxxx
+
+        //  POST to server containing chronicle-api
+        fetch({url: apiurl, body: log, headers: { method: "POST"}});
+    }
+}
